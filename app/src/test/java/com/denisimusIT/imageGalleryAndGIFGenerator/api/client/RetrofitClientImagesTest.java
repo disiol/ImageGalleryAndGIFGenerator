9e1f36e1a54a06eb7919c52a519bd190f908c9bc
develop.qt@gmail.com <LenovoA706>
2019-02-02 14:34:57 +0200

    Crate new test

--- app/src/test/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/RetrofitClientImagesTest.java
+++ app/src/test/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/RetrofitClientImagesTest.java
@@ -4,6 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
@@ -37,6 +38,23 @@ public class RetrofitClientImagesTest {
         String expected = "{\"error\":\"Invalid access token\"}";
         String actual = retrofitClient.serverApi.getGif().execute().errorBody().string();
         assertEquals("getGifTestErrorIdntLogin",expected, actual);
+    }
+
+    @Ignore
+    @Test
+    public void addImageTestErrorIdntLogin() throws IOException {
+        //TODO
+        RetrofitClient retrofitClient = new RetrofitClient();
+        File image = null;
+        String description = "";
+        String hashtag = "";
+        int latitude = 0;
+        int longitude = 0;
+
+        String expected = "{\"error\":\"Invalid access token\"}";
+
+        String actual = retrofitClient.serverApi.addImage(image, description, hashtag, latitude, longitude).toString();
+        assertEquals("addImageTestErrorIdntLogin",expected, actual);
     }
 
 }
