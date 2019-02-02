5012bfe30a22a5b6a4f4613770e5b8207da04afc
develop.qt@gmail.com <LenovoA706>
2019-02-02 15:19:10 +0200

    Coretc Interfese

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/ServerApi.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/ServerApi.java
@@ -3,6 +3,7 @@ package com.denisimusIT.imageGalleryAndGIFGenerator.api.client;
 import java.io.File;
 import java.lang.ref.Reference;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,20 +13,24 @@ import retrofit2.http.Query;
 //Authorization
 public interface ServerApi {
     @POST("create")
-    void createNewUser(@Query("username") String username, @Query("email") String email,
-                       @Query("password") String password, @Query("avatar") File avatar);
+        //TODO
+    Call<String> createNewUser(@Query("username") String username, @Query("email") String email,
+                               @Query("password") String password, @Query("avatar") File avatar);
 
     @POST("login")
-    Reference login(@Query("email") String email, @Query("password") String password);
+    Call<String> login(@Query("email") String email, @Query("password") String password);
 
     //Image
     @GET("all")
     Call<LinkedHashSet<String>> getAllUserImages();
 
     @GET("gif")
-    Call <String> getGif();
+    Call<String> getGif();
 
-    @POST("create")
-    String addImage(File image, String description, String hashtag, float latitude, float longitude);
+    @POST("image")
+        //TODO
+    Call<String> addImage(@Query("image") File image, @Query("description") String description,
+                          @Query("hashtag") String hashtag, @Query("latitude") float latitude,
+                          @Query("longitude") float longitude);
 
 }
