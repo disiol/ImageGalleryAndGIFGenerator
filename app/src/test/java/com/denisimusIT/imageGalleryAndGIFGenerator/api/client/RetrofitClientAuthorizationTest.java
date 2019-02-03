6e3a8d5704f54cfac64f84c64da612ef0c5efe65
develop.qt@gmail.com <LenovoA706>
2019-02-03 19:01:10 +0200

    refactoring CrateNewUserErrorEmptyFieldsTest

--- app/src/test/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/RetrofitClientAuthorizationTest.java
+++ app/src/test/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/client/RetrofitClientAuthorizationTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class RetrofitClientImagesAuthorizationTest {
+public class RetrofitClientAuthorizationTest {
 
     @Test
     public void CrateNewUserErrorEmptyFieldsTest() throws IOException {
