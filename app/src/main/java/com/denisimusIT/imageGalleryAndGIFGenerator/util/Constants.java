6ee9f36f5858cb0e8bbcf68cfb6ecbe0f55470b7
develop.qt@gmail.com <LenovoA706>
2019-02-06 19:30:21 +0200

    I made contract for table LoginData

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/util/Constants.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/util/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
     public final static String BASE_API_URL = "http://api.doitserver.in.ua/";
 
     public static final String IMAGE_MEDIA_TYPE = "image/*";
+    public static final String MY_LOG = "MyLog";
 
 }
