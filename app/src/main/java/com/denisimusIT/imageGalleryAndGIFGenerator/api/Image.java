55f5266f1a53d186b440ad0da61924df921835a3
develop.qt@gmail.com <LenovoA706>
2019-02-06 21:34:59 +0200

    add picasso

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/Image.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/api/Image.java
@@ -1,21 +1,17 @@
 package com.denisimusIT.imageGalleryAndGIFGenerator.api;
 
-import com.denisimusIT.imageGalleryAndGIFGenerator.api.client.App;
 import com.denisimusIT.imageGalleryAndGIFGenerator.api.client.dto.GetAllUserImages;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
 
-import retrofit2.Call;
 import retrofit2.Response;
 
 public class Image {
    public Response<GetAllUserImages> getAddedImages(String token) throws IOException {
        //TODO
 
-        Response<GetAllUserImages> actual = App.getApi().getAllUserImages(token).execute();
 
-       return actual;
+       return null;
    }
 
     public  void addANewImage(){
