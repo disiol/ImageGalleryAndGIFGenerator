b821edd36723af83462059481e888fba33a1f0b0
develop.qt@gmail.com <LenovoA706>
2019-02-09 11:22:10 +0200

    add email.isEmpty() || password.isEmpty check

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/screean/authorization/signIn/LoginParser.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/screean/authorization/signIn/LoginParser.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.denisimusIT.imageGalleryAndGIFGenerator.R;
 import com.denisimusIT.imageGalleryAndGIFGenerator.api.client.RetrofitClient;
 import com.denisimusIT.imageGalleryAndGIFGenerator.api.client.dto.UserDTO;
 
@@ -15,7 +16,11 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.*;
+import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.addDataToTableLoginData;
+import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.clearTable;
+import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.getAllDataFromTableLoginData;
+import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.getAvatarDataFromTableLoginData;
+import static com.denisimusIT.imageGalleryAndGIFGenerator.db.DatabaseComands.getCreationTimeDataFromTableLoginData;
 import static com.denisimusIT.imageGalleryAndGIFGenerator.util.AppUtill.showToastError;
 import static com.denisimusIT.imageGalleryAndGIFGenerator.util.Constants.LOG_TAG;
 import static com.denisimusIT.imageGalleryAndGIFGenerator.util.FileUtils.getImageForAvatar;
@@ -27,55 +32,61 @@ class LoginParser {
 
 
     public String login(String email, String password, final ImageView imageViewAvatar, final TextView textViewUserName, final Context login) {
-        //TODO
-        retrofitClient.serverApi.login(email, password).enqueue(new Callback<UserDTO>() {
-            @Override
-            public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
-                if (response.isSuccessful()) {
 
-                    clearTable(login);
-                    responseLogin = response.body().toString();
-                    Log.d(LOG_TAG, "login response: " + responseLogin);
+        if (email.isEmpty() || password.isEmpty()){
+            showToastError(login,login.getString(R.string.eror_empty_fileds));
+        }else {
+            //TODO
+            retrofitClient.serverApi.login(email, password).enqueue(new Callback<UserDTO>() {
+                @Override
+                public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
+                    if (response.isSuccessful()) {
 
-                    String avatarImageLink = response.body().getAvatarImageLink();//TODO get from table user data;
-                    String creationTime = response.body().getCreationTime();
-                    String token = response.body().getToken();
+                        clearTable(login);
+                        responseLogin = response.body().toString();
+                        Log.d(LOG_TAG, "login response: " + responseLogin);
 
-                    addDataToTableLoginData(login, avatarImageLink, creationTime, token);
-                    getAllDataFromTableLoginData(login);
+                        String avatarImageLink = response.body().getAvatarImageLink();
+                        String creationTime = response.body().getCreationTime();
+                        String token = response.body().getToken();
 
-                    String avatarDataFromTableLoginData = getAvatarDataFromTableLoginData(login);
+                        addDataToTableLoginData(login, avatarImageLink, creationTime, token);
+                        getAllDataFromTableLoginData(login);
 
-                    Uri imageURI = Uri.parse(avatarDataFromTableLoginData);
-                    getImageForAvatar(imageURI, imageViewAvatar);
-                    textViewUserName.setText(getCreationTimeDataFromTableLoginData(login));
-                    //TODO вызвать PicturesList
+                        String avatarDataFromTableLoginData = getAvatarDataFromTableLoginData(login);
 
-                } else {
-                    try {
+                        Uri imageURI = Uri.parse(avatarDataFromTableLoginData);
+                        getImageForAvatar(imageURI, imageViewAvatar);
+                        textViewUserName.setText(getCreationTimeDataFromTableLoginData(login));
+                        //TODO вызвать PicturesList
 
-                        responseLogin = response.errorBody().string();
-                        //TODO finish the text of an error
-                        showToastError(login, responseLogin);
-                        Log.e(LOG_TAG, "login errorBody: " + responseLogin);
+                    } else {
+                        try {
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                            responseLogin = response.errorBody().string();
+                            //TODO finish the text of an error
+                            showToastError(login, responseLogin);
+                            Log.e(LOG_TAG, "login errorBody: " + responseLogin);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
+
+
                 }
 
+                @Override
+                public void onFailure(Call<UserDTO> call, Throwable t) {
+                    //TODO  finish the text of an error err connect to internet
+                    showToastError(login, t.toString());
+                    Log.e(LOG_TAG, "login errorBody: " + t.toString());
 
-            }
+                }
 
-            @Override
-            public void onFailure(Call<UserDTO> call, Throwable t) {
-                //TODO  finish the text of an error err connect to internet
-                showToastError(login, t.toString());
-                Log.e(LOG_TAG, "login errorBody: " + t.toString());
+            });
 
-            }
-
-        });
+        }
 
         return responseLogin;  // for tests;
     }
