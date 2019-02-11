05933a60e8e6fd41754b2bd3666d7d4ab9783b9b
develop.qt@gmail.com <LenovoA706>
2019-02-11 14:05:19 +0200

    mace Register stricture.

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/screean/authorization/signUp/Register.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/screean/authorization/signUp/Register.java
@@ -2,16 +2,44 @@ package com.denisimusIT.imageGalleryAndGIFGenerator.screean.authorization.signUp
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.denisimusIT.imageGalleryAndGIFGenerator.R;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener  {
+    TextView textViewImageViewSignUp;
+    EditText editTextUserName, editTextEmail, editTextPassword, editTextConfimPassWord;
+    ImageView imageViewRegister;
+    Button buttonRegistrationSignUp;
+    ProgressBar progressBarRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        imageViewRegister = findViewById(R.id.imageView_sign_up);
+        editTextUserName = findViewById(R.id.et_user_name);
+        editTextEmail = findViewById(R.id.et_email);
+        editTextPassword = findViewById(R.id.et_password);
+        editTextConfimPassWord = findViewById(R.id.et_confirm_password);
+        buttonRegistrationSignUp = findViewById(R.id.bt_registration_sign_up);
+        progressBarRegister = findViewById(R.id.progressBarLogin);
+
+        imageViewRegister.setOnClickListener(this);
+
+
         //TODO проверку на зполненость обязательніх полей и вод ошибок
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
