37c268da1364335cb87224ce3cf6f423abfdc7d8
develop.qt@gmail.com <LenovoA706>
2019-02-14 00:30:45 +0200

    add prepareFilePart

---
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/util/ApiUtils.java
@@ -0,0 +1,35 @@
+package com.denisimusIT.imageGalleryAndGIFGenerator.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URLConnection;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
+public class ApiUtils extends DialogFragment {
+    public static void showToastError(Context context, String errorText) {
+        Toast toast = Toast.makeText(context, errorText, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    private RequestBody createPartFromString(String descriptionString) {
+        return RequestBody.create(
+                okhttp3.MultipartBody.FORM, descriptionString);
+    }
+
+    @NonNull
+    public static MultipartBody.Part prepareFilePart(String partName, File file) {
+
+        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+        RequestBody requestFile = RequestBody.create(MediaType.parse(mimeType), file);
+
+        // MultipartBody.Part is used to send also the actual file name
+        return MultipartBody.Part.createFormData(partName, file.getName(), requestFile);
+    }
+}
