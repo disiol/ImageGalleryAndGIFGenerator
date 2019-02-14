16951ea3eb72161bf1a496c36f652d245b638b71
develop.qt@gmail.com <LenovoA706>
2019-02-14 16:41:07 +0200

    add  email validator

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/util/RecvestsParser.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/util/RecvestsParser.java
@@ -1,17 +1,9 @@
 package com.denisimusIT.imageGalleryAndGIFGenerator.util;
 
-import android.net.Uri;
-import android.util.Log;
-
 import java.io.File;
 
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static com.denisimusIT.imageGalleryAndGIFGenerator.util.Constants.IMAGE_MEDIA_TYPE;
 
@@ -20,7 +12,7 @@ public class RecvestsParser {
         return RequestBody.create(MediaType.parse("text/plain"), string);
     }
 
-    public static RequestBody getImageRequestBody(String imageFile) {
+    public static RequestBody getImageRequestBody(File imageFile) {
         return RequestBody.create(MediaType.parse(IMAGE_MEDIA_TYPE), imageFile);
     }
 
