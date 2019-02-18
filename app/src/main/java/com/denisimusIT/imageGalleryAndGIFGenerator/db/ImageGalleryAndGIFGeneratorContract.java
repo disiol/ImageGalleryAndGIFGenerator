0095134836ebdd3b2a4a99136412cc6c83fb311c
develop.qt@gmail.com <LenovoA706>
2019-02-18 21:09:36 +0200

    mace ImageData contract

--- app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/db/ImageGalleryAndGIFGeneratorContract.java
+++ app/src/main/java/com/denisimusIT/imageGalleryAndGIFGenerator/db/ImageGalleryAndGIFGeneratorContract.java
@@ -15,8 +15,12 @@ public class ImageGalleryAndGIFGeneratorContract {
     public static final class ImageData {
         public final static String TABLE_NAME = "imageData";
         public final static String _ID = BaseColumns._ID;
-        public final static String COLUMN_TOKEN = "token";
-        public final static String COLUMN_AVATAR = "avatar";
-        public final static String COLUMN_CREATION_TIME = "creationTime";
+        public final static String COLUMN_ID = "id";
+        public final static String COLUMN_LONGITUDE = "longitude";
+        public final static String COLUMN_ADDRESS = "address";
+        public final static String COLUMN_WEATHER = "weather";
+        public final static String COLUMN_LATITUDE = "latitude";
+        public final static String COLUMN_SMALL_IMAGE_URL_PATH = "smallImageUrlPath";
+        public final static String BIG_IMAGE_URL_PATH = "bigImageUrlPath";
     }
 }
